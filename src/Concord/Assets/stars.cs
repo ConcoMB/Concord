c1ce9c629e37bfe37c241aa3ee8cc0a615071402
Conco MB <cmaderblanco@wolox.com.ar>
2015-10-18 21:48:26 -0300

    1

---
+++ src/Concord/Assets/stars.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class stars : MonoBehaviour {
+
+	public float startSpeed = 12;
+	private int level = 1;
+
+	void Start() {
+		GameObject enemyspawner = GameObject.Find ("enemyspawner");
+		SetupSpeed ();
+	}
+
+	void SetupSpeed() {
+		particleSystem.startSpeed = Random.Range (startSpeed * level, startSpeed * level * 1.5f);
+	}
+
+	void  levelup ( int lvlNumber  ){
+		level = lvlNumber;
+		SetupSpeed ();
+	}
+}
